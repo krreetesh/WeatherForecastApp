19ce32a3f825ccdd9f08175e12a88d4055e90fcb
Reetesh Kumar <reeteshkumar@shanmugams-macbook-pro-4.local>
2018-05-09 14:24:16 +0530

    Added Storyboard scenes and navigate from MainViewController to TableViewController

---
+++ WeatherForecastApp/MainViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MainViewController.swift
+//  WeatherForecastApp
+//
+//  Created by Reetesh Kumar on 09/05/18.
+//  Copyright © 2018 Reetesh Kumar. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet var txtCityName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "segueForTableView" {
+            if let city=txtCityName.text{
+                let vc = segue.destination as! TableViewController
+                vc.title = "5 day Weather Forecast in "+"\(city)"
+            }
+        }
+    }
+
+
+}
+
